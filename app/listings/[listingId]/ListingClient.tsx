5603c3ceda48b7b48513d5e36d16b2312e675bb2
avestar <avestar121@gmail.com>
2023-06-06 19:36:37 +0200

    fixed booking price

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -103,7 +103,7 @@ const ListingClient: React.FC<ListingClientProps> = ({
       );
       
       if (dayCount && listing.price) {
-        setTotalPrice(dayCount * listing.price);
+        setTotalPrice(dayCount * listing.price + listing.price);
       } else {
         setTotalPrice(listing.price);
       }
